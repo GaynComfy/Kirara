e864b30285300e040e7932c391294f945fafcf58
Liz3 <accs@liz3.net>
2020-11-24 23:21:33 +0100

    always run if 0 or negative

--- src/utils/withCooldown.js
+++ src/utils/withCooldown.js
@@ -1,4 +1,8 @@
 module.exports = async (cache, id, command, handler, cd = 25) => {
+  if (cd <= 0) {
+    await handler();
+    return;
+  }
   if (await cache.exists(`cmdcooldown:${id}:${command}`)) return;
   await handler();
   await cache.setExpire(`cmdcooldown:${id}:${command}`, "1", cd);
