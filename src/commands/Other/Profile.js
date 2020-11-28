3433a481ecd57ac3ee4fab324f7117e66b39ff7e
Liz3 <accs@liz3.net>
2020-11-28 02:53:03 +0100

    AAAAAANNND

--- src/commands/Other/Profile.js
+++ src/commands/Other/Profile.js
@@ -29,7 +29,7 @@ module.exports = {
     const {
       rows: [position],
     } = await instance.database.pool.query(
-      "SELECT aggregates.* FROM (SELECT count(id) AS c, discord_id, ROW_NUMBER() OVER (ORDER BY count(id) DESC) as row FROM card_claims WHERE claimed=true server_id=$2 group by discord_id ORDER BY c desc) as aggregates WHERE aggregates.discord_id=$1",
+      "SELECT aggregates.* FROM (SELECT count(id) AS c, discord_id, ROW_NUMBER() OVER (ORDER BY count(id) DESC) as row FROM card_claims WHERE claimed=true AND server_id=$2 group by discord_id ORDER BY c desc) as aggregates WHERE aggregates.discord_id=$1",
       [member.id, instance.serverIds[message.guild.id]]
     );
 
