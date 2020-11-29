7829818f8eb777bc94c28fd604a504612d6f9c2c
Liz3 <accs@liz3.net>
2020-11-29 02:55:36 +0100

    added claim notifier

---
+++ src/events/spawn-notifier.js
@@ -0,0 +1,31 @@
+const { tierInfo } = require("../utils/cardUtils");
+const allowed = ["4", "5", "6"];
+module.exports = {
+  execute: async (instance, message) => {
+    if (message.author.id !== "673362753489993749") {
+      return;
+    }
+    for (const embed of message.embeds) {
+      const word = embed.title;
+
+      if (!word || !word.includes("Tier:")) {
+        continue;
+      }
+      const parts = word.split(" Tier: ");
+      const tier = parts[parts.length - 1];
+      if (!allowed.includes(tier)) return;
+      const result = await instance.database.simpleQuery("CARD_ROLES", {
+        tier: `t${tier}`,
+        server_id: instance.serverIds[message.guild.id],
+      });
+      if (result.rows.length === 1) {
+        await message.channel.send(
+          `${tierInfo[`T${tier}`].emoji} <@&${
+            result.rows[0].role_id
+          }> | \`T${tier} has spawned!\``
+        );
+      }
+    }
+  },
+  eventName: "message",
+};
