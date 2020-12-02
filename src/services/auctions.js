0659021a75d9273c3e2ecb9cd55473926c8c81fb
JeDaYoshi <hi@jeda.im>
2020-12-02 05:11:26 -0400

    starting bid fix

--- src/services/auctions.js
+++ src/services/auctions.js
@@ -38,7 +38,7 @@ module.exports = {
             `${tier.emoji} [${data.card_name} T${data.tier}]` +
               `(https://animesoul.com/cards/info/${data.card_id}) V${data.version} is being auctioned!`
           )
-          .addField("Starting Bid", `富${data.bn * 0.21625}`, true)
+          .addField("Starting Bid", `富${Math.round(data.bn * 0.21)}`, true)
           .addField("Buy Now", `富${data.bn}`, true)
           .addField("Min. Increment", `+富${data.minimum}`, true);
 
