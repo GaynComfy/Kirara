00cb58159fa4526e077ce918676d3cb845e15de5
Liz3 <accs@liz3.net>
2020-12-08 05:51:14 +0100

    ths is stupid

--- src/commands/Shoob/Market.js
+++ src/commands/Shoob/Market.js
@@ -121,8 +121,16 @@ const processWithoutCard = async (instance, message) => {
 
     const market = result.map(
       (listing) =>
-        `> • \`${listing.item.name} T${listing.item.tier}\` | ` +
-        `> [• \`${listing.item.issue}\`](https://animesoul.com/market) | ` +
+        `> • \`${
+          listing.item.name.length >= 20
+            ? listing.item.name.substr(0, 20)
+            : listing.item.name +
+              Array(20 - listing.item.name.length)
+                .fill()
+                .map((e) => " ")
+                .join("")
+        } T${listing.item.tier}\` | ` +
+        `[• \`${listing.item.issue}\`](https://animesoul.com/market) | ` +
         `\`富 ${listing.price}\` | ` +
         ` \`${moment(listing.date_added * 1000).fromNow()}\``
     );
