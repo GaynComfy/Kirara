be78b4e853791adb65cfa201a3a5fd38edbcecfa
JeDaYoshi <hi@jeda.im>
2020-12-14 18:11:41 -0400

    I think I will actually commit this other file

---
+++ src/commands/Roleplay/Neko.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require("discord.js");
+const Fetcher = require("../../utils/GifFetcher");
+const { generateRolePlayEmbed } = require("./utils");
+const { withCount } = require("../../utils/rolePlayHooks");
+const Color = require("../../utils/Colors.json");
+const info = {
+  name: "neko",
+  matchCase: false,
+  category: "Roleplay",
+  cooldown: 60,
+};
+module.exports = {
+  execute: async (instance, message, args) => {
+    const { url } = await Fetcher.request("neko");
+    const embed = new MessageEmbed()
+      .setDescription("Nyaa~")
+      .setColor(Color.white);
+    if (
+      instance.settings[message.guild.id][`roleplay_size:${message.channel.id}`]
+    )
+      embed.setThumbnail(url);
+    else embed.setImage(url);
+    await message.channel.send(embed);
+    return true;
+  },
+  info,
+  help: {
+    usage: "neko",
+    examples: ["neko"],
+    description: "Posts a cuke neko",
+  },
+};
