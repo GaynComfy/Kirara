c327708e89db4ff64cdc819e56dd252d562d8fd1
JeDaYoshi <hi@jeda.im>
2020-12-18 22:39:36 -0400

    fix

--- src/commands/Information/Donate.js
+++ src/commands/Information/Donate.js
@@ -9,7 +9,7 @@ const info = {
 const InviteEmbed = new MessageEmbed()
   .setAuthor("Kirara", "https://cdn.comfy.gay/a/kMjAyMC0wMQ.png")
   .setDescription(
-    "<:Flame:7834392935065191 You can donate through Tomi to help us keep working on the bot\nUsing `as!send 77256980288253952 <amount>`" +
+    "<:Flame:7834392935065191> You can donate through Tomi to help us keep working on the bot\nUsing `as!send 77256980288253952 <amount>`" +
       "\n\nhttps://donatebot.io/checkout/378599231583289346 also has a donate tier for Sirona-Kirara!"
   )
   .setColor("#570489");
