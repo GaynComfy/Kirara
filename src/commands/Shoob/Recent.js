1eda05e58f1dec5326725aa9951643cd0ebee598
JeDaYoshi <hi@jeda.im>
2020-12-22 14:50:39 -0400

    add reverse on recent

--- src/commands/Shoob/Recent.js
+++ src/commands/Shoob/Recent.js
@@ -14,6 +14,10 @@ const allowed = ["t1", "t2", "t3", "t4", "t5", "t6"];
 
 module.exports = {
   execute: async (instance, message, args) => {
+    const reverse =
+      args.length >= 1 &&
+      (args[0].toLowerCase() === "reverse" || args[0].toLowerCase() === "r");
+    if (reverse) args.shift();
     const isGlobal =
       args.length >= 1 &&
       (args[0].toLowerCase() === "global" || args[0].toLowerCase() === "g");
@@ -56,7 +60,7 @@ module.exports = {
         }`
       );
     if (recentCards.length !== 0) {
-      recentCards.reverse();
+      if (!reverse) recentCards.reverse();
 
       const claimers = [];
       for (const item of recentCards) {
@@ -96,8 +100,8 @@ module.exports = {
   },
   info,
   help: {
-    usage: "recent [global] [T1/T2/T3/T4/T5/T6]",
-    examples: ["recent t1", "recent g t6"],
+    usage: "recent [reverse] [global] [T1/T2/T3/T4/T5/T6]",
+    examples: ["recent t1", "recent g t6", "r r"],
     description: "Show last cards spawned by Shoob",
   },
 };
