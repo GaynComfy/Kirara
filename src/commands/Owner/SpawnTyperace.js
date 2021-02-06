b4f6e41f007c69881df0b9655fba98453cf7f1c6
JeDaYoshi <hi@jeda.im>
2021-02-06 08:47:13 -0400

    oh

--- src/commands/Owner/SpawnTyperace.js
+++ src/commands/Owner/SpawnTyperace.js
@@ -104,7 +104,7 @@ module.exports = {
         )}`;
         const txt = captcha.token;
         const captchaImg = await loadImage(buffer);
-        ctx.drawImage(captchaImg, 21, 360, 258, 70);
+        ctx.drawImage(captchaImg, 21, 359, 258, 67);
 
         // the fake spawn
         const attachment = new MessageAttachment(canvas.toBuffer(), "name.png");
