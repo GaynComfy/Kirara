f318b3938cfe8542351660312005f82910cd9380
JeDaYoshi <hi@jeda.im>
2021-02-26 16:48:05 -0400

    Changes in auctions/games

--- src/services/games.js
+++ src/services/games.js
@@ -10,9 +10,87 @@ const tierSettings = {
   6: { emoji: "<:NewT6:781684992937558047>", num: 6, color: "#ff170f" },
 };
 const allowed = ["3", "4", "5", "6", "S"];
+const allAllowed = ["2"].concat(allowed);
+
 let client = null;
 let deleteInterval = null;
 const deleteMap = {};
+
+const onMessage = async (channel, message) => {
+  if (channel !== "games") return;
+
+  const data = JSON.parse(message);
+  if (
+    // normal cases
+    !allowed.includes(data.tier) &&
+    // ToDo: change this to checker if the user/server has voted in top.gg
+    process.env.NODE_ENV === "development" &&
+    !allAllowed.includes(data.tier)
+  )
+    return;
+
+  const tier = tierSettings[data.tier];
+  const embed = new MessageEmbed()
+    .setTitle(`> <:SShoob:783636544720207903> Enter the Minigame`)
+    .setURL(`https://animesoul.com/mini-game/${data.id}`)
+    .setColor(tier.color)
+    .setThumbnail(`https://animesoul.com/api/cardr/${data.card_id}`)
+    .setDescription(
+      `${tier.emoji} [\`${data.card_name}\` • \`T${data.tier}\`]` +
+        `(https://animesoul.com/cards/info/${data.card_id}) • \`V${data.version}\` is on a minigame!`
+    );
+
+  const guilds = instance.client.guilds.cache
+    .array()
+    .filter((g) => instance.settings[g.id]["games_channel"]);
+
+  for (const guild of guilds) {
+    const logChannel = guild.channels.cache.get(
+      instance.settings[guild.id]["games_channel"]
+    );
+    if (logChannel) {
+      const autodel = instance.settings[guild.id]["games_autodelete"];
+      try {
+        const msg = await logChannel.send(embed);
+        if (autodel) {
+          deleteMap[msg.id] = {
+            msg,
+            time: Date.now() + parseInt(autodel) * 60 * 1000,
+          };
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      // channel doesn't exists, delete from notifications
+      const {
+        rows: [result],
+      } = await instance.database.simpleQuery("SETTINGS", {
+        key: `games_channel`,
+        guild_id: guild.id,
+      });
+      if (!result) return;
+      const {
+        rows: [autodel],
+      } = await instance.database.simpleQuery("SETTINGS", {
+        key: `games_autodelete`,
+        guild_id: guild.id,
+      });
+
+      await instance.database.simpleDelete("SETTINGS", {
+        id: result.id,
+      });
+      delete instance.settings[guild.id][`games_channel`];
+      if (autodel) {
+        await instance.database.simpleDelete("SETTINGS", {
+          id: autodel.id,
+        });
+        delete instance.settings[guild.id][`games_autodelete`];
+      }
+    }
+  }
+};
+
 module.exports = {
   start: async (instance) => {
     deleteInterval = setInterval(async () => {
@@ -29,72 +107,7 @@ module.exports = {
       `redis://${config.cache.host}:${config.cache.port}`
     );
     client.subscribe("games");
-    client.on("message", async (channel, message) => {
-      if (channel === "games") {
-        const data = JSON.parse(message);
-        if (!allowed.includes(data.tier)) return;
-        const tier = tierSettings[data.tier];
-        const embed = new MessageEmbed()
-          .setTitle(`> <:SShoob:783636544720207903> Enter the Minigame`)
-          .setURL(`https://animesoul.com/mini-game/${data.id}`)
-          .setColor(tier.color)
-          .setThumbnail(`https://animesoul.com/api/cardr/${data.card_id}`)
-          .setDescription(
-            `${tier.emoji} [\`${data.card_name}\` • \`T${data.tier}\`]` +
-              `(https://animesoul.com/cards/info/${data.card_id}) • \`V${data.version}\` is on a minigame!`
-          );
-
-        const guilds = instance.client.guilds.cache
-          .array()
-          .filter((g) => instance.settings[g.id]["games_channel"]);
-
-        for (const guild of guilds) {
-          const logChannel = guild.channels.cache.get(
-            instance.settings[guild.id]["games_channel"]
-          );
-          if (logChannel) {
-            const autodel = instance.settings[guild.id]["games_autodelete"];
-            try {
-              const msg = await logChannel.send(embed);
-              if (autodel) {
-                deleteMap[msg.id] = {
-                  msg,
-                  time: Date.now() + parseInt(autodel) * 60 * 1000,
-                };
-              }
-            } catch (err) {
-              console.error(err);
-            }
-          } else {
-            // channel doesn't exists, delete from notifications
-            const {
-              rows: [result],
-            } = await instance.database.simpleQuery("SETTINGS", {
-              key: `games_channel`,
-              guild_id: guild.id,
-            });
-            if (!result) return;
-            const {
-              rows: [autodel],
-            } = await instance.database.simpleQuery("SETTINGS", {
-              key: `games_autodelete`,
-              guild_id: guild.id,
-            });
-
-            await instance.database.simpleDelete("SETTINGS", {
-              id: result.id,
-            });
-            delete instance.settings[guild.id][`games_channel`];
-            if (autodel) {
-              await instance.database.simpleDelete("SETTINGS", {
-                id: autodel.id,
-              });
-              delete instance.settings[guild.id][`games_autodelete`];
-            }
-          }
-        }
-      }
-    });
+    client.on("message", onMessage);
   },
   stop: async (instance) => {
     if (deleteInterval) clearInterval(deleteInterval);
