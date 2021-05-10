73785303156865742e8c6a98bc3a14e700c73ed1
JeDaYoshi <hi@jeda.im>
2021-05-10 00:29:10 -0400

    prettify optout

---
+++ src/commands/Other/OptOut.js
@@ -0,0 +1,60 @@
+const { MessageEmbed } = require("discord.js");
+const Color = require("../../utils/Colors.json");
+const info = {
+  name: "lb-optout",
+  aliases: ["optout"],
+  matchCase: false,
+  category: "Information",
+};
+
+module.exports = {
+  execute: async (instance, message, args) => {
+    const embed = new MessageEmbed().setColor(Color.default);
+    const { id: discord_id } = message.author;
+    const {
+      rows: [setting],
+    } = await instance.database.simpleQuery("USER_SETTINGS", {
+      discord_id,
+      key: "lb-optout",
+    });
+
+    if (args.length === 0) {
+      const status = setting ? "opted out" : "opted in";
+      embed.setDescription(
+        `<a:Sirona_loading:748854549703426118> <@!${discord_id}> is currently \`${status}\` from leaderboards.`
+      );
+    } else if (args.length === 1) {
+      const arg = args[0].toLowerCase();
+
+      if (arg === "yes") {
+        if (!setting)
+          await instance.database.simpleInsert("USER_SETTINGS", {
+            discord_id,
+            key: "lb-optout",
+            value: "y",
+          });
+      } else if (arg === "no") {
+        if (setting)
+          await instance.database.simpleDelete("USER_SETTINGS", {
+            id: setting.id,
+          });
+      } else return false;
+
+      const status = setting ? "opted in" : "opted out";
+      embed
+        .setDescription(
+          `<a:Sirona_Tick:749202570341384202> <@!${discord_id}> is now \`${status}\` from leaderboards.`
+        )
+        .setColor("RANDOM");
+    } else return false;
+
+    return message.channel.send(embed);
+  },
+  info,
+  help: {
+    usage: "lb-optout [yes/no]",
+    examples: ["lb-optout yes", "lb-optout no"],
+    description:
+      "Use to see & change if you are opted out of global leaderboards",
+  },
+};
