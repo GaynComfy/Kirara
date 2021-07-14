7214d70ec9619995dc5ad731529e53cb12b13cc0
JeDaYoshi <hi@jeda.im>
2021-07-14 15:22:32 -0400

    actually

--- src/events/boost-tracker.js
+++ src/events/boost-tracker.js
@@ -1,14 +1,18 @@
 module.exports = {
   execute: async (instance, oldMember, newMember) => {
     if (!newMember.guild || newMember.guild.id !== "378599231583289346") return;
+    const channel = newMember.guild.channels.cache.get("704084460336971906");
+    if (!channel) {
+      console.error("bot config channel not found for member stop boost");
+      return;
+    }
     if (oldMember.premiumSince && !newMember.premiumSince) {
-      const channel = newMember.guild.channels.cache.get("704084460336971906");
-      if (!channel) {
-        console.error("bot config channel not found for member stop boost");
-        return;
-      }
       await channel.send(
-        `User: ${newMember.user.username}[${newMember.id}] Stopped boosting.`
+        `User <@!${newMember.user.id}> [${newMember.user.id}] stopped boosting.`
+      );
+    } else {
+      await channel.send(
+        `User <@!${newMember.user.id}> [${newMember.user.id}] is now boosting.`
       );
     }
   },
