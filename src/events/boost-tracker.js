fe5b947a7cf572790d74e189e85617bbb21428ab
JeDaYoshi <hi@jeda.im>
2021-07-14 15:20:52 -0400

    so this has been broken for a while and nobody noticed

--- src/events/boost-tracker.js
+++ src/events/boost-tracker.js
@@ -2,7 +2,7 @@ module.exports = {
   execute: async (instance, oldMember, newMember) => {
     if (!newMember.guild || newMember.guild.id !== "378599231583289346") return;
     if (oldMember.premiumSince && !newMember.premiumSince) {
-      const channel = newMember.guild.channel.cache.get("704084460336971906");
+      const channel = newMember.guild.channels.cache.get("704084460336971906");
       if (!channel) {
         console.error("bot config channel not found for member stop boost");
         return;
