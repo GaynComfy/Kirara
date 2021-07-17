f0ecb3ef00f2d8583813140800db6d36a61f4c49
JeDaYoshi <hi@jeda.im>
2021-07-16 21:22:03 -0400

    ratelimits should be error

--- src/events/RateLimit.js
+++ src/events/RateLimit.js
@@ -1,9 +1,9 @@
 module.exports = {
   execute: async (instance, params) => {
     if (instance.client.rest.globalTimeout) {
-      console.debug(`[${instance.client.shard.ids[0]}] ! GLOBAL RATELIMIT HIT`);
+      console.error(`[${instance.client.shard.ids[0]}] ! GLOBAL RATELIMIT HIT`);
     }
-    console.debug(
+    console.error(
       `[${instance.client.shard.ids[0]}] ! Hit ratelimit on '${params.method} ${params.route}' => timeout ${params.timeout}`
     );
   },
