f43dfc6af2b74750ec3092534846c957cdeeb67e
JeDaYoshi <hi@jeda.im>
2021-08-03 01:08:01 -0400

    revert changes on boost-tracker for the time being

--- src/events/boost-tracker.js
+++ src/events/boost-tracker.js
@@ -1,5 +1,3 @@
-const humanizeDuration = require("humanize-duration");
-
 module.exports = {
   execute: async (instance, oldMember, newMember) => {
     if (!newMember.guild || newMember.guild.id !== "378599231583289346") return;
@@ -8,34 +6,13 @@ module.exports = {
       console.error("bot config channel not found for member stop boost");
       return;
     }
-
-    const now = Date.now();
-    const settings = { round: true, units: ["y", "mo", "w", "d", "h", "m"] };
-    const oldSince = humanizeDuration(
-      now - oldMember.premiumSinceTimestamp || 0,
-      settings
-    );
-    const newSince = humanizeDuration(
-      now - newMember.premiumSinceTimestamp || 0,
-      settings
-    );
-
     if (oldMember.premiumSince && !newMember.premiumSince) {
-      // user stopped boosting
       await channel.send(
-        `User <@!${newMember.user.id}> [${newMember.user.id}] stopped boosting after ${oldSince}.`
+        `User <@!${newMember.user.id}> [${newMember.user.id}] stopped boosting.`
       );
     } else if (newMember.premiumSince && !oldMember.premiumSince) {
-      // user is now boosting
       await channel.send(
-        `User <@!${newMember.user.id}> [${newMember.user.id}] is now boosting. (${newSince})`
-      );
-    } else if (
-      oldMember.premiumSinceTimestamp > newMember.premiumSinceTimestamp
-    ) {
-      // timestamp for booster changed (boost change)
-      await channel.send(
-        `User <@!${newMember.user.id}> [${newMember.user.id}] changed boost time from ${oldSince} to ${newSince}.`
+        `User <@!${newMember.user.id}> [${newMember.user.id}] is now boosting.`
       );
     }
   },
