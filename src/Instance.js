c85b2f6c513721c42b34486493efc4ee1f830c0c
JeDaYoshi <hi@jeda.im>
2021-08-29 02:57:03 -0400

    add another fix to reload

--- src/Instance.js
+++ src/Instance.js
@@ -78,6 +78,7 @@ class Instance {
       name => name.endsWith(".js")
     );
     for (const file of entries) {
+      delete require.cache[`./${file}`];
       const event = require(`./${file}`);
       if (event.disabled) continue;
       if (!event.eventName) throw new Error(`no event name specified! ${file}`);
@@ -102,6 +103,7 @@ class Instance {
       name => name.endsWith(".js")
     );
     for (const file of entries) {
+      delete require.cache[`./${file}`];
       const event = require(`./${file}`);
       if (event.disabled) continue;
       if (!event.stop || !event.start)
@@ -120,6 +122,7 @@ class Instance {
       name => name.endsWith(".js") && !name.endsWith("utils.js")
     );
     for (const file of entries) {
+      delete require.cache[`./${file}`];
       const command = require(`./${file}`);
       if (commands[command.info.name]) {
         throw new Error(`Duplicated command ${command.info.name}`);
