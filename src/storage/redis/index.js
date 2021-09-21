48643cdc74976658c79d8c2b7f4197ec55ba865e
Rajeev Athreya <rajeev@jawaji.com>
2021-09-21 16:17:31 +1000

    reverted some redis related things

--- src/storage/redis/index.js
+++ src/storage/redis/index.js
@@ -1,18 +1,10 @@
 const Redis = require("ioredis");
 const RedisApi = require("./Api");
 
-module.exports.RedisAPI = config => {
+module.exports = config => {
   return new Promise((resolve, reject) => {
     const client = new Redis(config);
     client.once("ready", () => resolve(new RedisApi(client, config)));
-    client.once("error", error => reject(error));
-  });
-};
-
-module.exports.RedisEvents = config => {
-  return new Promise((resolve, reject) => {
-    const client = new Redis(config);
-    client.once("ready", () => resolve(client));
     client.once("error", error => reject(error));
   });
 };
