9f4f72bd57249bb26bd731628d21e2bfb373ec3b
JeDaYoshi <hi@jeda.im>
2021-11-15 07:47:49 -0400

    add fallback roleplay pics

--- src/utils/GifFetcher.js
+++ src/utils/GifFetcher.js
@@ -2,10 +2,24 @@ const axios = require("axios").default;
 
 class GifFetcher {
   constructor() {
-    this.map = {};
+    // add some fallbacks in case we lag on start
+    this.map = {
+      hug: { url: "https://i.waifu.pics/HWhukIP.gif" },
+      neko: { url: "https://i.waifu.pics/Qyr5N-~.png" },
+      pat: { url: "https://i.waifu.pics/vAcG_hT.gif" },
+      bonk: { url: "https://i.waifu.pics/XCxAgkz.gif" },
+      wave: { url: "https://i.waifu.pics/Jvi3~TN.gif" },
+      yeet: { url: "https://i.waifu.pics/9CeCIw4.gif" },
+      awoo: {
+        url: "https://cdn.discordapp.com/attachments/690981388299665478/808348212456194108/alydrip.png",
+      },
+      lick: { url: "https://i.waifu.pics/p0FcuY4.gif" },
+      kiss: { url: "https://i.waifu.pics/ROlkkMZ.gif" },
+      handhold: { url: "https://i.waifu.pics/gsFX6IT.gif" },
+    };
     this.instance = axios.create({
       baseURL: "https://api.waifu.pics/sfw",
-      timeout: 500,
+      timeout: 400, // it should take less than a roundtrip to the other side of the world
     });
   }
   request(type) {
@@ -18,6 +32,7 @@ class GifFetcher {
         })
         .catch(err => {
           if (this.map[type]) {
+            console.error(err);
             resolve(this.map[type]);
             return;
           }
