538e2280a7253a0adfbb6e274e9591b99dc695fc
Nota30 <harenkeshaav11@gmail.com>
2021-11-18 11:20:42 +0600

    added intents

--- src/discord/index.js
+++ src/discord/index.js
@@ -1,11 +1,17 @@
-const Discord = require("discord.js");
+const { Client, Intents } = require("discord.js");
 
 module.exports = (token = process.env.TOKEN) => {
-  const client = new Discord.Client({
+  const client = new Client({
     allowedMentions: {
       parse: ["users"],
       repliedUser: true,
     },
+    intents: [
+      Intents.FLAGS.GUILDS,
+      Intents.FLAGS.GUILD_MESSAGES,
+      Intents.FLAGS.GUILD_MESSAGE_TYPING,
+      Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    ],
     presence: {
       status: "idle",
       activities: [{ name: "starting up...", type: 3 }],
