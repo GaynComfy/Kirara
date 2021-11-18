7b4935ef77957aa4192c2c1d5a65548fc0273276
JeDaYoshi <hi@jeda.im>
2021-11-18 11:02:42 -0400

    update intents
    we're not currently using GUILD_WEBHOOKS, but the plan is to add webhooks for
    auctions/minigames notifications in the future to reduce API ratelimits

--- src/discord/index.js
+++ src/discord/index.js
@@ -1,17 +1,22 @@
 const { Client, Intents } = require("discord.js");
 
 module.exports = (token = process.env.TOKEN) => {
+  const intents = [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_WEBHOOKS,
+  ];
+  if (process.env.NODE_ENV === "development") {
+    intents.push(Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_INTEGRATIONS);
+  }
+
   const client = new Client({
     allowedMentions: {
       parse: ["users"],
       repliedUser: true,
     },
-    intents: [
-      Intents.FLAGS.GUILDS,
-      Intents.FLAGS.GUILD_MESSAGES,
-      Intents.FLAGS.GUILD_MESSAGE_TYPING,
-      Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-    ],
+    intents,
     presence: {
       status: "idle",
       activities: [{ name: "starting up...", type: 3 }],
