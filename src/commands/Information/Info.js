55a130542e29b2a15abf789708186aa2e9125765
Nota <harenkeshaav11@gmail.com>
2021-11-20 22:07:04 +0600

    I changed my tag :)

--- src/commands/Information/Info.js
+++ src/commands/Information/Info.js
@@ -8,7 +8,7 @@ const embed = new MessageEmbed()
     "Multi-purpose bot for Shoob activities with extras! Completely free with no restrictions. " +
       "Part of [__**Gay & Comfy**__](https://discord.gg/comfy)!"
   )
-  .addField("Owners:", "`Nota#1576`, `⋆˚🌺⃤ Jorgy₊ ˚#2024`, `Alycans#1693`")
+  .addField("Owners:", "`Nota#8888`, `⋆˚🌺⃤ Jorgy₊ ˚#2024`, `Alycans#1693`")
   .addField("Lead Dev:", "[`JeDaYoshi#7942`](https://jeda.im)")
   .addField(
     "Team and Collaborators:",
