16162edf4f283cba4dacfb8a38e0bd1150dede2b
JeDaYoshi <hi@jeda.im>
2022-02-21 07:02:53 -0400

    pg pool tuning

--- src/storage/database/index.js
+++ src/storage/database/index.js
@@ -2,18 +2,26 @@ const { Pool } = require("pg");
 const PgApi = require("./Api");
 
 module.exports = (config, isProd) => {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     const connectionProps = {
       user: config.database.user,
       host: config.database.host,
       database: config.database.database,
       port: config.database.port,
+      max: 3,
+      idleTimeoutMillis: 300000,
     };
     if (isProd || config.database.password || process.env.PG_PASSWORD) {
       connectionProps.password =
         config.database.password || process.env.PG_PASSWORD || null;
     }
     const pool = new Pool(connectionProps);
-    resolve(new PgApi(pool, config.database));
+    pool
+      .connect()
+      .then(client => {
+        client.release();
+        resolve(new PgApi(pool, config.database));
+      })
+      .catch(reject);
   });
 };
