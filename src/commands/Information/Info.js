53466d9f867a91bf6fcd36553e68d5f86000fb65
Bappy <koehn40308@gmail.com>
2022-06-13 00:06:19 -0400

    Update Info.js

--- src/commands/Information/Info.js
+++ src/commands/Information/Info.js
@@ -11,11 +11,11 @@ const embed = new MessageEmbed()
     "Multi-purpose bot for Shoob activities with extras! Completely free with no restrictions. " +
       "Part of [__**Gay & Comfy**__](https://discord.gg/comfy)!"
   )
-  .addField("Owners:", "`Nota#8888`, `⋆˚🌺⃤ Jorgy₊ ˚#2024`, `Alycans#1693`")
+  .addField("Owners:", "`Nota#8888`, `⋆˚🌺⃤ Jorgy₊ ˚#5611`, `Alycans#1693`")
   .addField("Lead Dev:", "[`JeDaYoshi#7942`](https://jeda.im)")
   .addField(
     "Team and Collaborators:",
-    "`JeDaYoshi#7942`, `Liz3#0001`, `⋆˚🌺⃤ Jorgy₊ ˚#2024`, `Nota#1576`, " +
+    "`JeDaYoshi#7942`, `Liz3#0001`, `⋆˚🌺⃤ Jorgy₊ ˚#5611`, `Nota#8888`, `bappy#3311`" +
       "`offbeatwitch#8860`, `Riku#1111`"
   )
   .setFooter({ text: "Roleplay images provided by https://waifu.pics" });
