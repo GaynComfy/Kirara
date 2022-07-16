fd077f3a60b323047a5fc798293936196ce9a59c
Cassakura <90803273+Cassakura@users.noreply.github.com>
2022-07-16 13:24:38 -0400

    Simple Shoob Vote Command (#72)

---
+++ src/services/shoobvote.js
@@ -0,0 +1,27 @@
+let interval = null;
+
+module.exports = {
+  start: async instance => {
+    if (interval !== null) return;
+    if (!instance.shared["shoobv"]) instance.shared["shoobv"] = [];
+
+    interval = setInterval(async () => {
+      for (const user in instance.shared["shoobv"]) {
+        const time = instance.shared["shoobv"][user];
+        if (Date.now() - time.last < 43200000) continue;
+        const userH = await instance.client.users.fetch(time.user);
+        if (userH) {
+          await userH.send("You can now vote for Shoob again!");
+        }
+        const i = instance.shared["shoobv"].indexOf(time);
+        if (i !== -1) instance.shared["shoobv"].splice(i, 1);
+      }
+    }, 600000);
+  },
+  stop: async () => {
+    if (interval !== null) {
+      clearInterval(interval);
+      interval = null;
+    }
+  },
+};
