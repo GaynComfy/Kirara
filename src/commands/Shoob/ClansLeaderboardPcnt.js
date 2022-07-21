ba43b77d9e459f812b1f7a720b9690ea3799b28a
JeDaYoshi <hi@jeda.im>
2022-07-21 18:51:56 -0400

    add clbp, misc changes

--- src/commands/Shoob/ClansLeaderboardPcnt.js
+++ src/commands/Shoob/ClansLeaderboardPcnt.js
@@ -6,8 +6,8 @@ const { owner } = isDev
   : require("../../config-prod.js");
 
 const info = {
-  name: "clansleaderboard",
-  aliases: ["clb"],
+  name: "clansleaderboardpcnt",
+  aliases: ["clbp"],
   matchCase: false,
   category: "Shoob",
   cooldown: 2,
@@ -16,45 +16,42 @@ const info = {
 
 const clans = {
   "Stygian Rogues": "789481061389434900",
-  // Oasis: "813217659772076043",
+  Oasis: "813217659772076043",
   "Forsaken Clan": "813251025867243551",
   Frost: "855985381115953163",
 };
-// don't sue me for this
-const allClans = Object.assign({}, clans, {
-  Oasis: "813217659772076043",
-});
-const penalties = [0, 0, 20, 0];
+const dearUsers = [
+  "505420217342754816",
+  "475505022986092556",
+  "473465372461236244",
+];
+const penalties = [0, 0, 0, 20];
 const eventStart = new Date(1657990800000);
 const eventStop = new Date(1661274000000);
 
 const clanNames = Object.keys(clans);
 const clanIds = Object.values(clans);
-const allClanNames = Object.keys(allClans);
-const allClanIds = Object.values(allClans);
 
 module.exports = {
-  execute: async (instance, message, args) => {
+  execute: async (instance, message) => {
     const time = Date.now();
-    const areWeUnfair =
-      args[0] === "unfair" && owner.includes(message.author.id);
+    const showUsers =
+      owner.includes(message.author.id) ||
+      dearUsers.includes(message.author.id);
     const running = time >= eventStart && time < eventStop;
-    const showAll = areWeUnfair || !running;
 
-    message.channel.sendTyping().catch(() => null);
-
-    if (instance.shared["knowsClans"] !== allClanIds.length) {
+    if (instance.shared["knowsClans"] !== clanIds.length) {
       // we need to fetch the server IDs from our guild ID list.
       // generally, they are not supposed to mutate, so this is fine to do once
       // while not hardcoding stuff... outside of the IDs themselves.
-      let query = `SELECT id, guild_id FROM SERVERS WHERE ${allClanIds
+      let query = `SELECT id, guild_id FROM SERVERS WHERE ${clanIds
         .map(g => `guild_id='${g}'`)
         .join(" OR ")}`;
       const { rows } = await instance.database.pool.query(query);
       rows.forEach(srv => (instance.serverIds[srv.guild_id] = srv.id));
-      instance.shared["knowsClans"] = allClanIds.length;
+      instance.shared["knowsClans"] = clanIds.length;
 
-      const missing = allClanIds.filter(
+      const missing = clanIds.filter(
         gid => instance.serverIds[gid] === undefined
       );
       if (missing.length !== 0) {
@@ -65,7 +62,7 @@ module.exports = {
     // we're gonna push the queries to an array to Promise.all them
     const claimAmtQueries = [];
     const claimersQueries = [];
-    (showAll ? allClanIds : clanIds).forEach(clan => {
+    clanIds.forEach(clan => {
       claimAmtQueries.push(
         instance.database.pool
           .query(
@@ -91,7 +88,7 @@ module.exports = {
     ]);
 
     // Get the information from clans, and sort it from most claims
-    const clans = (showAll ? allClanNames : clanNames)
+    const clans = clanNames
       .map((name, i) => {
         const c = claims[i];
         const claimd = c.find(f => f.claimed === true);
@@ -115,52 +112,47 @@ module.exports = {
         };
       })
       .sort((a, b) => {
-        if (a.camt > b.camt) return -1;
-        if (a.camt < b.camt) return 1;
+        if (a.percent > b.percent) return -1;
+        if (a.percent < b.percent) return 1;
         return 0;
       });
 
-    const fields = [];
+    let stats = "";
     for (const [i, clan] of clans.entries()) {
-      let value = "> - Nothing yet! <:Shoob:910973650042236938>";
-
-      if (clan.claimers.length !== 0) {
-        const users = [];
-        for (const [i, entry] of clan.claimers.entries()) {
-          const user = await instance.client.users.fetch(entry.discord_id);
-          const mention = user ? `\`${user.tag}\`` : `<@!${entry.discord_id}>`;
-          users.push(
-            `> ${
-              i === 0 ? "<a:Sirona_star:748985391360507924>" : ` **${i + 1}.** `
-            } ${mention} — **${entry.c} claims**`
-          );
-        }
-        value = users.join("\n");
-      }
-
-      let name = `${
+      let name = `\n> ${
         i === 0 ? "<a:Sirona_star:748985391360507924>" : `឵ **${i + 1}.** `
-      } ${clan.name} - ${clan.camt}/${clan.samt} claims (${clan.percent}%)`;
+      } ${clan.name} - ${clan.percent}% cards claimed`;
       if (clan.camt !== clan.tamt) {
-        name += ` [-${clan.penalty}, from ${clan.tamt} claims]`;
+        name += ` [-${clan.penalty}, from ${clan.tpercent}%`;
       }
+      stats += name;
 
-      fields.push({ name, value });
-    }
-    if (!showAll && clanNames.length !== allClanNames.length) {
-      let i = clanNames.length;
-      while (i < allClanNames.length) {
-        fields.push({
-          name: `឵ **X.**  ${allClanNames[i]} - not shown by clan's request`,
-          value: "> ---",
-        });
-        i++;
+      if (showUsers) {
+        let value = "> - Nothing yet! <:Shoob:910973650042236938>";
+        if (clan.claimers.length !== 0) {
+          const users = [];
+          for (const [i, entry] of clan.claimers.entries()) {
+            const user = await instance.client.users.fetch(entry.discord_id);
+            const mention = user
+              ? `\`${user.tag}\``
+              : `<@!${entry.discord_id}>`;
+            users.push(
+              `> - ${
+                i === 0
+                  ? "<a:Sirona_star:748985391360507924>"
+                  : ` **${i + 1}.** `
+              } ${mention} — **${entry.c} claims**`
+            );
+          }
+          value = users.join("\n");
+        }
+        stats += `\n${value}`;
       }
     }
 
     const embed = new EmbedBuilder()
       .setAuthor({
-        name: "Clan Wars Leaderboard",
+        name: "Clan Wars Percentage Leaderboard",
         iconURL: message.guild.iconURL({ dynamic: true }),
       })
       .setColor("#d5417c")
@@ -169,18 +161,26 @@ module.exports = {
           ? `<:Shoob:910973650042236938> **A WAR IS CURRENTLY ACTIVE!** <:Shoob:910973650042236938>\n`
           : "") +
           `> <t:${eventStart / 1000}:f> — <t:${eventStop / 1000}:f>` +
-          ` (<t:${eventStop / 1000}:R>)`
+          ` (<t:${eventStop / 1000}:R>)\n` +
+          stats
       )
-      .addFields(fields)
       .setImage(Constants.footer);
 
-    await message.channel.send({ embeds: [embed] });
+    try {
+      await message.author.send({ embeds: [embed] });
+      await message.react("✅").catch(() => null);
+    } catch (err) {
+      console.error(err);
+      return message.reply(
+        "Looks like I was unable to DM you. Please check you have Direct Messages enabled in your Privacy Settings!"
+      );
+    }
     return true;
   },
   info,
   help: {
-    usage: "clb",
-    examples: ["clb"],
-    description: "See the clan wars' global leaderboard!",
+    usage: "clbp",
+    examples: ["clbp"],
+    description: "See the clan wars' global percentage leaderboard!",
   },
 };
