5a8e3dc2ffa6c266c243095568632ef8e37b98e0
JeDaYoshi <hi@jeda.im>
2022-09-08 16:37:27 -0400

    lol

---
+++ src/commands/Owner/CCuddle.js
@@ -0,0 +1,50 @@
+const Fetcher = require("../../utils/GifFetcher");
+const { generateRolePlayEmbed } = require("./utils");
+const { withCount } = require("../../utils/rolePlayHooks.js");
+const info = {
+  name: "ccuddle",
+  matchCase: false,
+  category: "Owner",
+  cooldown: 5,
+};
+module.exports = {
+  execute: async (instance, message) => {
+    if (message.author.id !== "189978735816998913") {
+      return message.reply("Nah you ain’t Cass homie");
+    }
+    if (message.mentions.users.size === 0) {
+      return false;
+    }
+    const { url } = await Fetcher.request("cuddle", message.channel.id);
+    const embed = generateRolePlayEmbed(
+      "cuddles",
+      message.author.id,
+      message.mentions.users.first().id
+    );
+    if (
+      instance.settings[message.guild.id][`roleplay_size:${message.channel.id}`]
+    )
+      embed.setThumbnail(url);
+    else embed.setImage(url);
+    withCount(
+      instance,
+      "cuddle",
+      message.author.id,
+      message.mentions.users.first().id,
+      () => {
+        embed.setFooter({
+          text: "Now give me your fucking PayPal",
+        });
+        message.channel.send({ embeds: [embed] });
+      }
+    );
+
+    return true;
+  },
+  info,
+  help: {
+    usage: "ccuddle <@user>",
+    examples: ["ccuddle @Alycans"],
+    description: "Cuddles a user",
+  },
+};
