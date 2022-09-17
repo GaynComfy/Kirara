faab0525896708b987856aad5b3747adc3612724
Cassakura <90803273+Cassakura@users.noreply.github.com>
2022-09-17 03:15:45 -0400

    Roleplay Commands: Marry, Divorce

---
+++ src/commands/Roleplay/Divorce.js
@@ -0,0 +1,54 @@
+const { MessageCollector } = require("discord.js");
+
+const info = {
+  name: "divorce",
+  matchCase: false,
+  category: "Roleplay",
+  cooldown: 600,
+};
+module.exports = {
+  execute: async (instance, message) => {
+    const id = message.author.id;
+    //if (!isMarried) {
+    //  await message.channel.send(`<@${id}>, you are not married! What a lonely person!`);
+    //  return;
+    //} else {
+    await message.channel.send(
+      `Are you sure you want to give up on your relationship, <@${id}>?\nType "yes" to divorce or "no" to stay married!\n\n*You have one minute to reply!*`
+    );
+    const filter = async m => m.author.id === id;
+    const collector = new MessageCollector(message.channel, {
+      filter,
+      time: 60000,
+    });
+    collector.on("collect", async m => {
+      if (m.content.toLowerCase() === "yes") {
+        await message.channel.send(
+          `Yay, <@${id}>! You are now ~~lonely~~ single!`
+        );
+        collector.stop("final");
+        return;
+      } else if (m.content.toLowerCase() === "no") {
+        await message.channel.send(
+          `Nice, <@${id}>! You are still ~~trapped~~ married!`
+        );
+        collector.stop("final");
+        return;
+      }
+    });
+    collector.on("end", async () => {
+      if (collector.endReason !== "final") {
+        await message.channel.send(
+          `Time is up, <@${id}>! You still aren't single!`
+        );
+      }
+    });
+    //}
+  },
+  info,
+  help: {
+    usage: "divorce",
+    examples: ["divorce"],
+    description: "Requests to divorce",
+  },
+};
---
+++ src/commands/Roleplay/Marry.js
@@ -0,0 +1,61 @@
+const { MessageCollector } = require("discord.js");
+
+const info = {
+  name: "marry",
+  matchCase: false,
+  category: "Roleplay",
+  cooldown: 600,
+};
+module.exports = {
+  execute: async (instance, message) => {
+    const id = message.author.id;
+    const mentionedUser = message.mentions.users.first().id;
+    //const isMarried = instance.cache.exists(`married:${id}`);
+    if (message.mentions.users.size === 0) return;
+    if (id === mentionedUser) {
+      await message.channel.send("Hah, you're lonely... But no.");
+      return;
+    }
+    //if (isMarried) {
+    //  await message.channel.send(`<@${id}>, you are still married!`);
+    //  return;
+    //} else {
+    await message.channel.send(
+      `<@${mentionedUser}>, <@${id}> proposed to you!\nType "yes" to accept or "no" to decline!\n\n*You have one minute to reply!*`
+    );
+    const filter = async m => m.author.id === mentionedUser;
+    const collector = new MessageCollector(message.channel, {
+      filter,
+      time: 60000,
+    });
+    collector.on("collect", async m => {
+      if (m.content.toLowerCase() === "yes") {
+        await message.channel.send(
+          `<@${id}> and <@${mentionedUser}> are now married! Good luck...`
+        );
+        collector.stop("final");
+        return;
+      } else if (m.content.toLowerCase() === "no") {
+        await message.channel.send(
+          `<@${id}> was rejected by <@${mentionedUser}>! Hah, loser.`
+        );
+        collector.stop("final");
+        return;
+      }
+    });
+    collector.on("end", async () => {
+      if (collector.endReason !== "final") {
+        await message.channel.send(
+          `<@${id}> was ignored by <@${mentionedUser}>! That makes a lot of sense...`
+        );
+      }
+    });
+    //}
+  },
+  info,
+  help: {
+    usage: "marry <@user>",
+    examples: ["marry @cass"],
+    description: "Requests to marry a user",
+  },
+};
