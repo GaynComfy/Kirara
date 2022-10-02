99106b88d33abd7099bb2fa67271a21d276aca78
JeDaYoshi <hi@jeda.im>
2022-10-01 23:30:56 -0400

    this is better

--- src/commands/Roleplay/Marriage.js
+++ src/commands/Roleplay/Marriage.js
@@ -1,4 +1,5 @@
 const { getMarriage } = require("./utils");
+const { mention, userId } = require("../../utils/regexUtils");
 const Color = require("../../utils/Colors.json");
 const { EmbedBuilder } = require("discord.js");
 
@@ -10,19 +11,25 @@ const info = {
 };
 
 module.exports = {
-  execute: async (instance, message) => {
-    const asker =
-      message.mentions.users.size === 0
-        ? message.author
-        : message.mentions.users.first();
+  execute: async (instance, message, args) => {
+    let member =
+      message.mentions.users.first() ||
+      (args.length >= 1 &&
+        userId.test(args[0]) &&
+        (await instance.client.users.fetch(args[0]).catch(() => null)));
+    if (args.length >= 1 && (mention.test(args[0]) || userId.test(args[0])))
+      args.shift();
+    if (!member) {
+      member = message.author;
+    }
 
     const embed = new EmbedBuilder().setColor(Color.pink);
 
-    const marry = await getMarriage(instance, asker.id);
+    const marry = await getMarriage(instance, member.id);
     if (marry.length === 0) {
       embed
         .setDescription(
-          `<:Sirona_NoCross:762606114444935168> <@!${asker.id}> is not married to anyone!`
+          `<:Sirona_NoCross:762606114444935168> <@!${member.id}> is not married to anyone!`
         )
         .setColor(Color.red);
       await message.channel.send({ embeds: [embed] });
@@ -39,7 +46,7 @@ module.exports = {
       .join(",");
 
     embed.setDescription(
-      `<a:Sirona_star:748985391360507924> <@!${asker.id}> is married to ${married}`
+      `<a:Sirona_star:748985391360507924> <@!${member.id}> is married to ${married}`
     );
     await message.channel.send({ embeds: [embed] });
   },
