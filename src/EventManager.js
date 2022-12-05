9613bab9e38774c4e8efa6b36234cc5311ff2cd1
JeDaYoshi <hi@jeda.im>
2022-12-05 08:36:41 -0400

    god dammit

--- src/EventManager.js
+++ src/EventManager.js
@@ -10,8 +10,8 @@ const { owner } =
 
 const spaces = / +/g;
 
-const marriage = ["marriage", "marry", "divorce"];
-const rpBlacklist = ["850218927136571392", "873835108069679124"];
+/* const marriage = ["marriage", "marry", "divorce"];
+const rpBlacklist = ["850218927136571392", "873835108069679124"]; */
 
 class EventManager {
   /**
@@ -131,12 +131,12 @@ class EventManager {
     }
     if (command.info.guilds && !command.info.guilds.includes(message.guild.id))
       return; // return if we're not supposed to be used here
-    if (
+    /* if (
       command.info.category === "Roleplay" &&
       !marriage.includes(command.info.name) &&
       rpBlacklist.includes(message.author.id)
     )
-      return message.react("<a:DuckyNo:697121081999360087>").catch(() => null);
+      return message.react("<a:DuckyNo:697121081999360087>").catch(() => null); */
 
     const disabled =
       this.instance.settings[message.guild.id][
