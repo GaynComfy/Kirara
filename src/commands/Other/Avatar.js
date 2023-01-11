2c5fd0a3f787addef8dc914fc5faefcd8aa0b131
Liz3 <accs@liz3.net>
2023-01-11 06:43:05 +0100

    enhance avatar

--- src/commands/Other/Avatar.js
+++ src/commands/Other/Avatar.js
@@ -7,46 +7,50 @@ const info = {
   category: "UwU",
   cooldown: 60,
 };
-const mention = /<@!?(\d{17,19})>/;
-const userId = /\d{17,19}/;
+//const mention = /<@!?(\d{17,19})>/;
+//const userId = /\d{17,19}/;
 
+const getTarget = async (instance, message, args) => {
+  const target =
+    message.mentions.users[0] ||
+    (args.length ? await instance.client.users.fetch(args[0]) : message.author);
+  if (message.guild) {
+    const guildMember = await message.guild.members.fetch(target.id);
+    if (guildMember) {
+      if (guildMember.displayAvatarURL().length) return [guildMember, target];
+    }
+  }
+  return [target];
+};
 module.exports = {
   execute: async (instance, message, args) => {
-    let target =
-      message.mentions.users.first() ||
-      (args.length >= 1 &&
-        userId.test(args[0]) &&
-        (await instance.client.users.fetch(args[0]).catch(() => null)));
-    if (args.length >= 1 && (mention.test(args[0]) || userId.test(args[0])))
-      args.shift();
-    if (!target) {
-      target = message.author;
-    }
-
+    const targets = await getTarget(instance, message, args);
     // Embed
-    const embed = new EmbedBuilder()
-      .setTitle(`${target.tag}'s avatar`)
-      .setDescription(
-        `[PNG](${target.displayAvatarURL({
-          extension: "png",
-          size: 4096,
-        })}) | [JPG](${target.displayAvatarURL({
-          extension: "jpg",
-          size: 4096,
-        })}) | [WEBP](${target.displayAvatarURL({
-          extension: "webp",
-          size: 4096,
-        })})` +
-          (target.avatar.startsWith("a_")
-            ? ` | [GIF](${target.displayAvatarURL({
-                extension: "gif",
-                size: 4096,
-              })})`
-            : "")
-      )
-      .setColor("Random")
-      .setImage(target.displayAvatarURL({ size: 4096 }));
-    await message.channel.send({ embeds: [embed] });
+    const embeds = targets.map(target =>
+      new EmbedBuilder()
+        .setTitle(`${target.tag}'s avatar`)
+        .setDescription(
+          `[PNG](${target.displayAvatarURL({
+            extension: "png",
+            size: 4096,
+          })}) | [JPG](${target.displayAvatarURL({
+            extension: "jpg",
+            size: 4096,
+          })}) | [WEBP](${target.displayAvatarURL({
+            extension: "webp",
+            size: 4096,
+          })})` +
+            (target.avatar.startsWith("a_")
+              ? ` | [GIF](${target.displayAvatarURL({
+                  extension: "gif",
+                  size: 4096,
+                })})`
+              : "")
+        )
+        .setColor("Random")
+        .setImage(target.displayAvatarURL({ size: 4096 }))
+    );
+    await message.channel.send({ embeds });
   },
   info,
   help: {
