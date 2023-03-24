0f4f349804a74cae25c042d9ad1fda777028ba44
Liz3 <accs@liz3.net>
2023-03-24 02:24:13 +0100

    feat: anime command

---
+++ src/commands/Other/Anime.js
@@ -0,0 +1,80 @@
+const { EmbedBuilder } = require("discord.js");
+const { searchAnime } = require("../../utils/animeFetcher.js");
+const { createPagedResults } = require("../../utils/PagedResults");
+const info = {
+  name: "anime",
+  matchCase: false,
+  category: "UwU",
+};
+
+module.exports = {
+  execute: async (instance, message, args) => {
+    const term = args.join(" ");
+    const result = await searchAnime(instance, term);
+    if (!result) return;
+    const [anime] = result;
+
+    createPagedResults(
+      message,
+      2 + anime.getCharacters().length,
+      async page => {
+        const embed = new EmbedBuilder();
+        embed.setTitle(anime.getTitle());
+
+        if (page === 0) {
+          embed.setDescription(anime.getFormattedDescription());
+          embed.setImage(anime.getImageLink());
+          embed.addFields([
+            {
+              name: "Rating",
+              value: anime.getRating(),
+            },
+            {
+              name: "JP Title",
+              value: anime.getTitle("ja"),
+            },
+          ]);
+        } else if (page === 1) {
+          embed.setTitle(anime.getTitle() + ": Creators");
+          embed.setDescription(
+            anime
+              .getCreators()
+              .map(entry => `* ${entry.name}(${entry.type})`)
+              .join("\n")
+          );
+        } else {
+          const index = page - 2;
+          const character = anime.getCharacters()[index];
+          embed.setTitle("Character: " + character.name);
+          embed.setDescription(character.description);
+          embed.setImage(character.image);
+          embed.addFields([
+            {
+              name: "Rating",
+              value: character.rating,
+              inline: true,
+            },
+            {
+              name: "Gender",
+              value: character.gender,
+              inline: true,
+            },
+          ]);
+        }
+        embed.setFooter({
+          text: "Data provided by anidb",
+        });
+        return embed;
+      },
+      false,
+      null
+    );
+    return true;
+  },
+  info,
+  help: {
+    usage: "anime <name>",
+    examples: ["anime railgun"],
+    description: "Fetch Data about an anime",
+  },
+};
---
+++ src/utils/animeFetcher.js
@@ -0,0 +1,144 @@
+const axios = require("axios").default;
+const { parseString } = require("xml2js");
+class Anime {
+  constructor(data) {
+    this.data = data;
+  }
+  getTitle(lang = "en") {
+    const titles = this.data.titles[0].title;
+    if (!titles) return null;
+    for (const entry of titles) {
+      const title = entry._;
+      const meta = entry["$"];
+      if (meta.type === "official" && meta["xml:lang"] === lang) return title;
+    }
+    return null;
+  }
+  _imageLinkWithId(id) {
+    return `https://cdn-eu.anidb.net/images/main/${id}`;
+  }
+  getImageLink() {
+    const id = this.data.picture[0];
+    if (!id) return null;
+    return this._imageLinkWithId(id);
+  }
+  getRating() {
+    return this.data.ratings[0].permanent[0]?._;
+  }
+  getCharacters() {
+    if (!this.characters)
+      this.characters = this.data.characters[0].character.map(character => {
+        return {
+          name: character.name[0],
+          gender: character.gender?.[0],
+          id: character["$"]?.id,
+          description: character.description?.[0] || "",
+          image: this._imageLinkWithId(character.picture?.[0]),
+          rating: character.rating?.[0]._,
+        };
+      });
+    return this.characters;
+  }
+  getCreators() {
+    if (!this.creators)
+      this.creators = this.data.creators[0].name?.map(entry => {
+        const name = entry._;
+        const id = entry["$"].id;
+        const type = entry["$"].type;
+        return {
+          name,
+          id,
+          type,
+        };
+      });
+    return this.creators;
+  }
+  getFormattedDescription() {
+    if (this._formattedDescription) return this._formattedDescription;
+    const regex = /(http:\/\/anidb\.net\/c.+?) \[(.+?)\]/;
+    let description = this.data.description[0];
+    let newDescription = "";
+    let match;
+    while ((match = regex.exec(description))) {
+      if (!match) break;
+      const [full, url, name] = match;
+      const index = match.index;
+      if (index > 0) newDescription += description.substr(0, index);
+      newDescription += `[${name}](${url})`;
+      description = description.substr(index + full.length);
+    }
+    newDescription += description;
+    if (newDescription.startsWith("* "))
+      newDescription = newDescription.substr(2);
+    this._formattedDescription = newDescription;
+    console.log(newDescription);
+    return this._formattedDescription;
+  }
+}
+const parseXml = str => {
+  return new Promise((resolve, reject) => {
+    parseString(str, (err, res) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(res);
+    });
+  });
+};
+
+const fetchAnime = async aid => {
+  const result = await axios.get("http://api.anidb.net:9001/httpapi", {
+    params: {
+      client: process.env.ANIDB_CLIENT,
+      clientver: process.env.ANIDB_CLIENT_VERSION,
+      protover: "1",
+      request: "anime",
+      aid,
+    },
+  });
+  return parseXml(result.data);
+};
+
+const searchAnime = async (instance, term) => {
+  // const query = `select * from anime_list where (LOWER($1) LIKE ('%' || LOWER(title_en) || '%')) OR  (LOWER($1) LIKE ('%' || LOWER(title_jp) || '%')) LIMIT 5`;
+  const query = `select * from anime_list where LOWER(title_en) = LOWER($1) OR title_jp = $1 LIMIT 1`;
+  const { rows } = await instance.database.pool.query(query, [term]);
+  if (!rows.length) return null;
+  const mapped = await Promise.all(
+    rows.map(
+      row =>
+        new Promise((resolve, reject) => {
+          if (row.has_data) {
+            resolve(new Anime(row.data.anime));
+            return;
+          }
+          console.log("fetching anime", row.aid);
+          fetchAnime(row.aid)
+            .then(res => {
+              instance.database
+                .simpleUpdate(
+                  "ANIME_LIST",
+                  {
+                    id: row.id,
+                  },
+                  {
+                    has_data: true,
+                    data: JSON.stringify(res),
+                  }
+                )
+                .then(() => {
+                  resolve(new Anime(res.anime));
+                })
+                .catch(err => reject(err));
+            })
+            .catch(err => reject(err));
+        })
+    )
+  );
+  return mapped;
+};
+
+module.exports = {
+  searchAnime,
+};
